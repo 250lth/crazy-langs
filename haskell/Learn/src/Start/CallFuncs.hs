c58d3ed29647be9d9902cc0fbb905fda95d9fc21
250lth <gang250lth@gmail.com>
2017-03-30 23:00:41 +0800

    start using haskell functions

---
+++ haskell/Learn/src/Start/CallFuncs.hs
@@ -0,0 +1,11 @@
+module Start.CallFuncs where
+
+-- prefix functions
+a = succ 8
+b = min 9 10
+c = min 3.4 3.2
+d = max 100 101
+
+e = succ 9 + max 5 4 + 1
+f = div 92 10
+f1 = 92 `div` 10
---
+++ haskell/Learn/src/Start/WriteFunc.hs
@@ -0,0 +1,13 @@
+module Start.WriteFunc where
+
+doubleMe x = x + x
+
+doubleUs x y = x * 2 + y * 2
+
+doubleUs1 x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
