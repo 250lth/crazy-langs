e6579e47a36f6e63a77d46b80d8a76008ae2ff25
250lth <gang250lth@gmail.com>
2017-03-30 22:49:56 +0800

    begin haskell

--- haskell/Learn/src/Start/Begin.hs
+++ haskell/Learn/src/Start/Begin.hs
@@ -1,2 +1,18 @@
 module Start.Begin where
 
+main = print $ "Hello World"
+a = 2 + 15
+b = 49 * 100
+c = 5 / 2
+d = 50 * 100 - 4999
+e = 50 * (100 - 4999)
+
+b1 = True && False
+b2 = False || True
+b3 = not True
+
+c1 = 5 == 5
+c2 = 5 /= 5
+c3 = "hello" == "hello"
+
+-- d1 = 1 + "ah"
