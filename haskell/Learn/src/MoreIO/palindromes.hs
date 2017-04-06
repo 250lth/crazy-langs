1416b4f77208bc51cad01cc8d5d2d81d1f4ea972
250lth <gang250lth@gmail.com>
2017-04-06 15:25:54 +0800

    using haskell file io to get and use string

---
+++ haskell/Learn/src/MoreIO/palindromes.hs
@@ -0,0 +1,10 @@
+respondPalindromes :: String -> String
+respondPalindromes =
+    unlines .
+    map (\xs -> if isPal xs then "palindrome" else "not a palindrome")
+    . lines
+
+isPal :: String -> Bool
+isPal xs = xs == reverse xs
+
+main = interact respondPalindromes
