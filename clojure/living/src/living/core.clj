1d0bae30a2c2934150d078cde867a427ab13bac6
250lth <gang250lth@gmail.com>
2017-04-10 20:44:45 +0800

    create the living clojure project

---
+++ clojure/living/src/living/core.clj
@@ -0,0 +1,8 @@
+(ns living.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(foo "Rockets")
---
+++ clojure/living/test/living/core_test.clj
@@ -0,0 +1,7 @@
+(ns living.core-test
+  (:require [clojure.test :refer :all]
+            [living.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
