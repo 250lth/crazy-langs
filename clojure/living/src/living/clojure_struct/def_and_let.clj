991fc4ab4743b51b8872bf25856e3250fb9f59c3
250lth <gang250lth@gmail.com>
2017-04-11 20:19:24 +0800

    clojure def and let

---
+++ clojure/living/src/living/clojure_struct/def_and_let.clj
@@ -0,0 +1,13 @@
+(ns living.clojure-struct.def-and-let)
+
+;; def
+(def developer "Alice")
+developer
+living.clojure-struct.def-and-let/developer
+
+
+;; let
+(let [developer "Alice in Wonderland"]
+  developer)
+developer
+
