b9646e1ff24586a3c926e3640cd637d812522110
250lth <gang250lth@gmail.com>
2017-04-14 23:00:14 +0800

    add :aot to the project.clj package the project as a jar

--- clojure/living-samples/async-tea-party/project.clj
+++ clojure/living-samples/async-tea-party/project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.442"]])
+                 [org.clojure/core.async "0.3.442"]]
+  :main async-tea-party.tea-party-from-cmmandline
+  :aot [async-tea-party.tea-party-from-cmmandline])
