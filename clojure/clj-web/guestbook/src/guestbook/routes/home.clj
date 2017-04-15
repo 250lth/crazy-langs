b106836464aa9b7777a32d9b55b77ad873f85f6e
250lth <gang250lth@gmail.com>
2017-04-15 21:07:39 +0800

    use Hiccup to add form add POST method to the form

--- clojure/clj-web/guestbook/src/guestbook/routes/home.clj
+++ clojure/clj-web/guestbook/src/guestbook/routes/home.clj
@@ -1,17 +1,51 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home []
+(defn show-guests []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {:message "Hello" :name "Bob" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestamp]])])
+
+(defn home [& [name message error]]
   (layout/common
     [:h1 "Guestbook"]
     [:p "Welcome to my guestbook"]
+    [:p error]
+    ; here we call our show-guests function
+    ; to generate the list of existing comments
+    (show-guests)
     [:hr]
-    [:form
-     [:p "Name:"]
-     [:input]
-     [:p "Message:"]
-     [:textarea {:rows 10 :cols 40}]]))
+    ; here we create form with textfields called "name" and "message"
+    ; these will be sent when the form posts to the server as keywords of
+    ; the same name
+    (form-to [:post "/"]
+             [:p "Name"]
+             (text-field "name" name)
+             [:p "Message:"]
+             (text-area {:rows 10 :cols 40} "message" message)
+             [:br]
+             (submit-button "comment"))))
+
+(defn save-message [name message]
+  (cond
+    (empty? name)
+    (home name message "Some dummy forgot to leave me a name")
+    (empty? message)
+    (home name message "Don't you have something to say?")
+    :else
+    (do
+      (println name message)
+      (home))))
+
+
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (POST "/" [name message] (save-message name message)))
