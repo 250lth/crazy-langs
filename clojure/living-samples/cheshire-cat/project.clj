56d19d95a6efdd2ea85bc728eb17019892b41b06
250lth <gang250lth@gmail.com>
2017-04-15 18:56:17 +0800

    use clojurescript in ring app

--- clojure/living-samples/cheshire-cat/project.clj
+++ clojure/living-samples/cheshire-cat/project.clj
@@ -6,9 +6,21 @@
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [cheshire "5.6.3"]
-                 [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "1.9.293"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.4"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "resources/public/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true
+                                   }
+                        }]
+              })
