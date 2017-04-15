349bb54d79b337e4f66eb828774d93c6e65fe084
250lth <gang250lth@gmail.com>
2017-04-15 19:34:51 +0800

    use cljs and cljs-http to call http

--- clojure/living-samples/cheshire-cat/project.clj
+++ clojure/living-samples/cheshire-cat/project.clj
@@ -7,7 +7,9 @@
                  [ring/ring-defaults "0.2.1"]
                  [cheshire "5.6.3"]
                  [ring/ring-json "0.4.0"]
-                 [org.clojure/clojurescript "1.9.293"]]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [cljs-http "0.1.42"]
+                 [org.clojure/core.async "0.2.395"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-cljsbuild "1.1.4"]]
   :ring {:handler cheshire-cat.handler/app}
--- clojure/living-samples/cheshire-cat/src-cljs/core.cljs
+++ clojure/living-samples/cheshire-cat/src-cljs/core.cljs
@@ -1,5 +1,11 @@
 (ns cheshire-cat.core
-      (:require [clojure.browser.repl :as repl]))
+      (:require-macros [cljs.core.async.macros :refer [go]])
+      (:require [clojure.browser.repl :as repl]
+                [cljs-http.client :as http]
+                [cljs.core.async :refer [<!]]))
 
 (defn ^:export init []
-      (repl/connect "http://localhost:9000/repl"))
+      (repl/connect "http://localhost:9000/repl")
+      (go
+        (let [response (<! (http/get "/cheshire-cat"))]
+             (js/alert (:body response)))))
