e0d71b8dd507f5d05f6e2dab15e651db9dc42067
250lth <gang250lth@gmail.com>
2017-04-15 22:00:03 +0800

    add db to the app but i can only use the old version jdbc

--- clojure/clj-web/guestbook/project.clj
+++ clojure/clj-web/guestbook/project.clj
@@ -4,7 +4,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.2"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.4.0"]]
+                 [ring-server "0.4.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.16.1"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler guestbook.handler/app
          :init guestbook.handler/init
