289bb0c22b4fd31ac3f9ad806b150597e562ba6d
250lth <gang250lth@gmail.com>
2017-05-10 18:18:08 +0800

    way of ruby string expressions

---
+++ ruby/rubyist/string_symbol/string_express_methods.rb
@@ -0,0 +1,10 @@
+### quotes
+puts "Two plus two is #{2 + 2}"
+puts 'Two plus tow is #{2+2}'
+
+# other way, same as '', %q{}
+puts %q{you needn't escape apostrophes when using %q}
+# "" %Q{}
+
+### here doc
+# = <<
