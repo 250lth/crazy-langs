cd3b66257c8ee9c6ec67da7f303169c9900d51e7
250lth <gang250lth@gmail.com>
2017-05-11 21:04:14 +0800

    common way of writing orm

---
+++ ruby/metaprogramming/the_m_world/orm.rb
@@ -0,0 +1,63 @@
+# orm
+# map a table in the db to a class
+
+class Database
+  @sql = []
+
+  def self.sql(sql)
+    @sql << sql
+  end
+
+  def self.read_sql
+    @sql
+  end
+end
+
+
+class Entity
+  attr_reader :table, :ident
+
+  def initialize(table, ident)
+    @table = table
+    @ident = ident
+    Database.sql "INSERT INTO #{@table} (id) VALUES (#{@ident})"
+  end
+
+  def set(col, val)
+    Database.sql "UPDATE #{@table} SET #{col}='#{val}' WHERE id=#{@ident}"
+  end
+
+  def get(col)
+    Database.sql ("SELECT #{col} FROM #{@table} WHERE id=#{@ident}")[0][0]
+  end
+end
+
+
+# use Movie to map table -- movies
+class Movie < Entity
+  def initialize(ident)
+    super "movies", ident
+  end
+
+  def title
+    get "title"
+  end
+
+  def title=(value)
+    set "title", value
+  end
+
+  def director
+    get "director"
+  end
+
+  def director=(value)
+    set "director", value
+  end
+end
+
+
+### test
+movie = Movie.new(1)
+movie.title = "Doctor Strangelove"
+movie.director = "Stanley Kubrick"
