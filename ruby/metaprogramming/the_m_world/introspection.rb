e4fbfcaa75fe21efe019271cd353594b93fe4a46
250lth <gang250lth@gmail.com>
2017-05-11 20:39:44 +0800

    introspection to know ruby's language construct

---
+++ ruby/metaprogramming/the_m_world/introspection.rb
@@ -0,0 +1,15 @@
+class Greeting
+  def initialize(text)
+    @text = text
+  end
+
+  def welcome
+    @text
+  end
+end
+
+my_object = Greeting.new("Hello")
+
+p my_object.class
+p my_object.class.instance_methods(false)
+p my_object.instance_variables
