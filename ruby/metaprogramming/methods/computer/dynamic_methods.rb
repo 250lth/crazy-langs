00297d8ae5342948e87e9bc7d69365e74e5a7d83
250lth <gang250lth@gmail.com>
2017-05-12 22:01:43 +0800

    refactor again in dynamic methods and introspection

---
+++ ruby/metaprogramming/methods/computer/dynamic_methods.rb
@@ -0,0 +1,22 @@
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price >= 100
+      result
+    end
+  end
+
+  define_component :mouse
+  define_component :cpu
+  define_component :keyboard
+end
+
+require_relative 'unit_test'
