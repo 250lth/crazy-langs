e572d75d63cd3350dc20526dd60bfd2cbf3334e9
250lth <gang250lth@gmail.com>
2017-05-22 20:19:34 +0800

    rails tdd, writing tests!

--- ruby/rest-rails/hello_rails/test/controllers/say_controller_test.rb
+++ ruby/rest-rails/hello_rails/test/controllers/say_controller_test.rb
@@ -6,4 +6,13 @@ class SayControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should say hello rails in json" do
+    get :hello
+    assert_equal "{\"hello\":\"Rails!\"}", @response.body
+  end
+
+  test "should day hello john in json" do
+    get(:hello, {'user' => "John"})
+    assert_equal "{\"hello\":\"John!\"}", @response.body
+  end
 end
