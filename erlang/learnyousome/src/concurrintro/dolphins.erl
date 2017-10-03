7b0a1145dc872758f778d0ce953f6fade9ed70d3
250lth <gang250lth@gmail.com>
2017-10-03 11:01:21 +0800

    starting concurrency programming in erlang

---
+++ erlang/learnyousome/src/concurrintro/dolphins.erl
@@ -0,0 +1,46 @@
+%%%-------------------------------------------------------------------
+%%% @author lth
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. 十月 2017 上午10:05
+%%%-------------------------------------------------------------------
+-module(dolphins).
+-author("lth").
+-compile(export_all).
+
+%% API
+-export([]).
+
+dolphin1() ->
+  receive
+    do_a_flip ->
+      io:format("How about no?~n");
+    fish ->
+      io:format("So long and thanks for all the fish!~n");
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n")
+  end.
+
+dolphin2() ->
+  receive
+    {From, do_a_flip} ->
+      From ! "How about no?";
+    {From, fish} ->
+      From ! "So long and thanks for all the fish!";
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n")
+  end.
+
+dolphin3() ->
+  receive
+    {From, do_a_flip} ->
+      From ! "How about no?",
+      dolphin3();
+    {From, fish} ->
+      From ! "So long and thanks for all the fish!";
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n"),
+      dolphin3()
+  end.
