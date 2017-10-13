09ce2d9401c41163656749c546466f585a07c34f
250lth <gang250lth@gmail.com>
2017-10-13 21:42:24 +0800

    implement of partial, curry and compose

--- scala/fpscala/src/main/scala/start/PolymorphicFuncs.scala
+++ scala/fpscala/src/main/scala/start/PolymorphicFuncs.scala
@@ -24,6 +24,18 @@ object PolymorphicFuncs {
     go(0)
   }
 
+  def partial[A, B, C](a: A, f: (A, B) => C): (B => C) =
+    (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+
   def main(args: Array[String]): Unit = {
     println(findFirst(Array(7, 9, 13), (x: Int) => x == 9))
     println(isSorted(Array(3, 2, 1), (x:Int, y: Int) => x > y))
