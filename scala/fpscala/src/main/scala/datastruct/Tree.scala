9879a891f898a86fa79d2aeca9f03bfe1b61aa59
250lth <gang250lth@gmail.com>
2017-10-14 16:56:26 +0800

    using Fold to implement functions of Tree

--- scala/fpscala/src/main/scala/datastruct/Tree.scala
+++ scala/fpscala/src/main/scala/datastruct/Tree.scala
@@ -33,6 +33,27 @@ object Tree {
     }
   }
 
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = {
+    t match {
+      case Leaf(a) => f(a)
+      case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    }
+  }
 
+  def sizeViaFold[A](t: Tree[A]): Int = {
+    fold(t)(a => 1)(1 + _ + _)
+  }
+
+  def maximumViaFold(t: Tree[Int]): Int = {
+    fold(t)(a => a)(_ max _)
+  }
+
+  def depthViaFold[A](t: Tree[A]): Int = {
+    fold(t)(a => 0)((d1, d2) => 1 + (d1 max d2))
+  }
+
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = {
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
+  }
 
 }
