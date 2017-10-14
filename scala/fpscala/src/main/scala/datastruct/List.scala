adcc0bfa0a8a4cdf3ed77c4375900ed156a4cfb2
250lth <gang250lth@gmail.com>
2017-10-14 09:55:08 +0800

    starting defining the trait List

--- scala/fpscala/src/main/scala/datastruct/List.scala
+++ scala/fpscala/src/main/scala/datastruct/List.scala
@@ -22,17 +22,6 @@ object List {
     else Cons(as.head, apply(as.tail: _*))
   }
 
-  // sample
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
 
-  def main(args: Array[String]): Unit = {
-    println(x)
-  }
 
 }
---
+++ scala/fpscala/src/main/scala/datastruct/ListTest.scala
@@ -0,0 +1,19 @@
+package datastruct
+
+object ListTest {
+  import List._
+
+  // sample
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(x)
+  }
+
+}
