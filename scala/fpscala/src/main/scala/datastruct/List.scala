9aee067052ff77a28b17023852a41896227e42e8
250lth <gang250lth@gmail.com>
2017-10-14 16:24:11 +0800

    exercises of list in fp

--- scala/fpscala/src/main/scala/datastruct/List.scala
+++ scala/fpscala/src/main/scala/datastruct/List.scala
@@ -146,6 +146,59 @@ object List {
     foldRight(l, Nil: List[A])(append)
   }
 
+  def listAdd1(l: List[Int]): List[Int] = {
+    foldRight(l, Nil: List[Int])((h, t) => Cons(h+1, t))
+  }
 
+  def listDoubleToString(l: List[Double]): List[String] = {
+    foldRight(l, Nil:List[String])((h, t) => Cons(h.toString, t))
+  }
+
+  def map[A, B](as: List[A])(f: A => B): List[B] = {
+    foldRight(as, Nil: List[B])((h, t) => Cons(f(h), t))
+  }
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+    foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+  }
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    concat(map(as)(f))
+  }
+
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] = {
+    flatMap(l)(a => if (f(a)) List(a) else Nil)
+  }
+
+  def addPairList(a: List[Int], b: List[Int]): List[Int] = {
+    (a,b) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairList(t1, t2))
+    }
+  }
+
+  def zipWith[A, B, C](a: List[A], b: List[B])(f: (A, B) => C): List[C] =
+    (a, b) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    }
+
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean =
+    (l, prefix) match {
+      case (_, Nil) => true
+      case (Cons(h, t), Cons(h2, t2)) if h == h2 => startsWith(t, t2)
+      case _ => false
+    }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean =
+    sup match {
+      case Nil => sub == Nil
+      case _ if startsWith(sup, sub) => true
+      case Cons(h, t) => hasSubsequence(t, sub)
+    }
 
 }
