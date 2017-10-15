7c0aa3567981bb3967b8c285b71af667aebcf2a9
250lth <gang250lth@gmail.com>
2017-10-15 18:40:52 +0800

    Either type in scala

---
+++ scala/fpscala/src/main/scala/errorhandling/Either.scala
@@ -0,0 +1,56 @@
+package errorhandling
+
+import scala.{Option => _, Either => _}
+
+sealed trait Either[+E, +A] {
+
+  def map[B](f: A => B): Either[E, B] = {
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => f(a)
+    }
+  }
+
+  def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] = {
+    this match {
+      case Left(_) => b
+      case Right(a) => Right(a)
+    }
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    Either[EE, C] = for {a <- this; b1 <- b} yield f(a, b1)
+  }
+
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] = {
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+  }
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] = {
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+  }
+
+  def Try[A](a: => A): Either[Exception, A] = {
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+  }
+
+}
