a50fa6813246f299555f2ec42593e1f2c3ec4b04
250lth <gang250lth@gmail.com>
2017-10-25 11:05:15 +0800

    exercises of list and recursion

--- elixir/proframmingelixir/lib/lists/my_list.ex
+++ elixir/proframmingelixir/lib/lists/my_list.ex
@@ -9,6 +9,37 @@ defmodule MyList do
   def add1([]), do: []
   def add1([ head | tail]), do: [ head + 1 | add1(tail)]
 
+  def map([], _func), do: []
+  def map([ head | tail ], func), do: [ func.(head) | map(tail, func) ]
+
+  def sum(list), do: _sum(list, 0)
+
+  defp _sum([], total), do: total
+  defp _sum([ head | tail ], total), do: _sum(tail, head + total)
+
+  def sum2([]), do: 0
+  def sum2([ head | tail ]), do: head + sum2(tail)
+
+  def reduce([], value, _) do value end
+  def reduce([ head | tail ], value, func) do
+    reduce(tail, func.(head, value), func)
+  end
+
+  def mapsum([], _), do: 0
+  def mapsum([ head | tail ], f), do: f.(head) + mapsum(tail, f)
+
+  def max([ x ]), do: x
+  def max([ x, y | tail ]) when x > y, do: max([x | tail ])
+  def max([ x, y | tail ]) when x <= y, do: max([ y | tail ])
+
+  def caesar([], _n), do: []
+  def caesar([ head | tail ], n) do
+    [do_caesar(head, n) | caesar(tail, n)]
+  end
+
+  defp do_caesar(base, n) do
+    ?a + rem(base - ?a + n, ?z - ?a + 1)
+  end
 end
 
 MyList.len([]) |> IO.puts
@@ -24,3 +55,26 @@ IO.puts("==============================")
 
 MyList.add1([]) |> IO.inspect
 MyList.add1([1,2,3,4]) |> IO.inspect
+
+IO.puts "================================="
+MyList.map([1,2,3,4], fn (n) -> n * n end) |> IO.inspect
+MyList.map([1,2,3,4], fn (n) -> n + 1 end) |> IO.inspect
+MyList.map([1,2,3,4], fn (n) -> n > 2 end) |> IO.inspect
+MyList.map([1,2,3,4], &(&1 + 1)) |> IO.inspect
+
+IO.puts "==========================="
+MyList.sum([1,2,3,4,5]) |> IO.inspect
+MyList.sum2([1,2,3,4,5]) |> IO.inspect
+
+IO.puts "=============================="
+MyList.reduce([1,2,3,4,5], 0, &(&1 + &2)) |> IO.inspect
+MyList.reduce([1,2,3,4,5], 1, &(&1 * &2)) |> IO.inspect
+
+IO.puts "============================"
+MyList.mapsum [1,2,3,4,5], &(&1 * &1) |> IO.inspect
+
+IO.puts "============================"
+MyList.max([3,2,4,1,5,6,2]) |> IO.inspect
+
+IO.puts "============================"
+MyList.caesar('haaevkemyr', 13) |> IO.inspect
