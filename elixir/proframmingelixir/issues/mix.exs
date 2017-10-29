ab67b55dd4901102544ea03605f6eef0b8af6535
250lth <gang250lth@gmail.com>
2017-10-29 21:03:24 +0800

    add ex-doc to the project

--- elixir/proframmingelixir/issues/mix.exs
+++ elixir/proframmingelixir/issues/mix.exs
@@ -6,6 +6,7 @@ defmodule Issues.Mixfile do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
@@ -25,7 +26,9 @@ defmodule Issues.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       { :httpoison, "~>0.13.0" },
-      { :poison, "~>3.1.0" }
+      { :poison, "~>3.1.0" },
+      { :ex_doc, "~>0.18.1" },
+      { :earmark, "~>1.2.3", override: true }
     ]
   end
 
