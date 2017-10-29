0b8dcbf1eaaa6c061b93b35ac5568af4aa338654
250lth <gang250lth@gmail.com>
2017-10-29 16:44:54 +0800

    print the issues in table format

--- elixir/proframmingelixir/issues/lib/issues/cli.ex
+++ elixir/proframmingelixir/issues/lib/issues/cli.ex
@@ -1,5 +1,7 @@
 defmodule Issues.CLI do
 
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -47,11 +49,13 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_hashdicts
     |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
