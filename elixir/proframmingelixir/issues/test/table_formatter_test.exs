d1245748983700c1b0c2a3391868e5813bf6afcf
250lth <gang250lth@gmail.com>
2017-10-29 16:51:23 +0800

    print the issues in table format

--- elixir/proframmingelixir/issues/test/table_formatter_test.exs
+++ elixir/proframmingelixir/issues/test/table_formatter_test.exs
@@ -1,47 +1,48 @@
-defmodule Issues.TableFormatter do
+defmodule TableFormatterTest do
+  use ExUnit.Case          # bring in the test functionality
+  import ExUnit.CaptureIO  # And allow us to capture stuff sent to stdout
 
-  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
+  alias Issues.TableFormatter, as: TF
 
-  def print_table_for_columns(rows, headers) do
-    data_by_columns = split_into_columns(rows, headers)
-    column_widths   = widths_of(data_by_columns)
-    format          = format_for(column_widths)
-
-    puts_one_line_in_columns headers, format
-    IO.puts                  separator(column_widths)
-    puts_in_columns          data_by_columns, format
+  def simple_test_data do
+    [ [ c1: "r1 c1", c2: "r1 c2",  c3: "r1 c3", c4: "r1+++c4" ],
+      [ c1: "r2 c1", c2: "r2 c2",  c3: "r2 c3", c4: "r2 c4"   ],
+      [ c1: "r3 c1", c2: "r3 c2",  c3: "r3 c3", c4: "r3 c4"   ],
+      [ c1: "r4 c1", c2: "r4++c2", c3: "r4 c3", c4: "r4 c4"   ] ]
   end
 
-  def split_into_columns(rows, headers) do
-    for header <- headers do
-      for row <- rows, do: printable(row[header])
-    end
+  def headers, do: [ :c1, :c2, :c4 ]
+
+  def split_with_three_columns,
+      do: TF.split_into_columns(simple_test_data, headers)
+
+  test "split_into_columns" do
+    columns = split_with_three_columns
+    assert     length(columns) == length(headers)
+    assert List.first(columns) == ["r1 c1", "r2 c1", "r3 c1", "r4 c1"]
+    assert  List.last(columns) == ["r1+++c4", "r2 c4", "r3 c4", "r4 c4"]
   end
 
-  def printable(str) when is_binary(str), do: str
-  def printable(str), do: to_string(str)
-
-  def widths_of(columns) do
-    for column <- columns, do: column |> map(&String.length/1) |> max
+  test "column_widths" do
+    widths = TF.widths_of(split_with_three_columns)
+    assert widths == [ 5, 6, 7 ]
   end
 
-  def format_for(column_widths) do
-    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+  test "correct format string returned" do
+    assert TF.format_for([9, 10, 11]) == "~-9s | ~-10s | ~-11s~n"
   end
 
-  def separator(column_widths) do
-    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
-  end
-
-  def puts_in_columns(data_by_columns, format) do
-    data_by_columns
-    |> List.zip
-    |> map(&Tuple.to_list/1)
-    |> each(&puts_one_line_in_columns(&1, format))
-  end
-
-  def puts_one_line_in_columns(fields, format) do
-    :io.format(format, fields)
-  end
-
+  #test "Output is correct" do
+  #  result = capture_io fn ->
+  #    TF.print_table_for_columns(simple_test_data, headers)
+  #  end
+  #  assert result == """
+  #         c1    | c2     | c4
+  #         ------+--------+--------
+  #         r1 c1 | r1 c2  | r1+++c4
+  #         r2 c1 | r2 c2  | r2 c4
+  #         r3 c1 | r3 c2  | r3 c4
+  #         r4 c1 | r4++c2 | r4 c4
+  #         """
+  #end
 end
