8809ad98f0fbc300caee2bfd4a6f9fd9694c9338
250lth <gang250lth@gmail.com>
2017-10-30 20:34:38 +0800

    name the nodes, and send messages to every of the nodes

--- elixir/proframmingelixir/lib/nodes/ticker.ex
+++ elixir/proframmingelixir/lib/nodes/ticker.ex
@@ -5,7 +5,7 @@ defmodule Ticker do
   @name :ticker
 
   def start do
-    pid = spawn(__MODULE__, :generator, [[]])
+    pid = spawn(__MODULE__, :generator, [[], 0])
     :global.register_name(@name, pid)
   end
 
@@ -13,18 +13,25 @@ defmodule Ticker do
     send :global.whereis_name(@name), { :register, client_pid }
   end
 
-  def generator(clients) do
+  def generator(clients, next_client_index) do
+    client_count = length(clients)
+
+    if next_client_index > (client_count - 1) do
+      next_client_index = 0
+    end
+
     receive do
       { :register, pid } ->
         IO.puts "registering #{inspect pid}"
-        generator([pid|clients])
+        generator([pid|clients], next_client_index)
       after
         @interval ->
           IO.puts "tick"
-          Enum.each clients, fn client ->
-            send client, { :tick }
+          if client_count > 0 do
+            next_client = clients |> Enum.at(next_client_index)
+            send next_client, { :tick }
           end
-          generator(clients)
+          generator(clients, next_client_index + 1)
     end
   end
 
