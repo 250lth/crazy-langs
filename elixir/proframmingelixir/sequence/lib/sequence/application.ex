4f3e60a979759028fff5aa6f6f44846452cc3d85
250lth <gang250lth@gmail.com>
2017-10-31 09:57:39 +0800

    add supervisor to otp apps

---
+++ elixir/proframmingelixir/sequence/lib/sequence/application.ex
@@ -0,0 +1,22 @@
+defmodule Sequence.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Sequence.Worker.start_link(arg)
+      # {Sequence.Worker, arg},
+      worker(Sequence.Server, [123])
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Sequence.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
