7d25994ffb1a46986af3e412608859b77554adba
250lth <gang250lth@gmail.com>
2017-10-31 08:48:00 +0800

    add apis to app, name the pid

--- elixir/proframmingelixir/sequence/lib/sequence/server.ex
+++ elixir/proframmingelixir/sequence/lib/sequence/server.ex
@@ -25,4 +25,18 @@ defmodule Sequence.Server do
     [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
   end
 
+  ###
+  # APIs
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call __MODULE__, :next_number
+  end
+
+  def increment_number(delta) do
+    GenServer.cast __MODULE__, {:increment_number, delta}
+  end
+
 end
--- elixir/proframmingelixir/sequence/lib/stack/server.ex
+++ elixir/proframmingelixir/sequence/lib/stack/server.ex
@@ -5,6 +5,24 @@ defmodule Stack.Server do
 
   use GenServer
 
+  ####
+  ## APIs
+
+  def start_link(current_stack) do
+    GenServer.start_link(__MODULE__, current_stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(element) do
+    GenServer.cast(__MODULE__, { :push, element })
+  end
+
+  ####
+  # GenServer implementation
+
   def handle_call(:pop, _from, [ head | tail ]) do
     { :reply, head, tail }
   end
@@ -13,6 +31,11 @@ defmodule Stack.Server do
     new_list = List.insert_at(current_stack, 0, new_element)
 
     { :noreply, new_list }
+  end
+
+  def terminate(reason, state) do
+    IO.puts "reason: #{inspect reason}\n
+              state: #{inspect state}"
   end
 
 end
