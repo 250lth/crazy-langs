0e2f491d4e2f7c977293c081cc05969b59494714
250lth <gang250lth@gmail.com>
2017-11-02 18:50:51 +0800

    learn plugs in phoenix

---
+++ elixir/hello/lib/hello_web/controllers/message_controller.ex
@@ -0,0 +1,44 @@
+defmodule HelloWeb.MessageController do
+  @moduledoc false
+  
+  use HelloWeb, :controller
+
+  plug :put_headers, %{content_encoding: "gzip", cache_control: "max-age=3600"}
+  plug :put_layout, "bare.html"
+
+  plug :authenticate
+  plug :find_message
+  plug :authorize_message
+
+  def show(conn, params) do
+    render conn, :show, page: find_message(params["id"])
+  end
+
+  def authenticate(conn, _) do
+    case Authenticator.find_user(conn) do
+      {:ok, user} ->
+        assign(conn, :user, user)
+      :error ->
+        conn |> put_flash(:info, "You must be logged in") |> redirect(to: "/") |> halt()
+    end
+  end
+
+  defp find_message(id), do: ...
+  defp find_message(conn, _) do
+    case find_message(conn.params["id"]) do
+      nil ->
+        conn |> put_flash(:info, "That message wasn't found") |> redirect(to: "/") |> halt()
+      message ->
+        assign(conn, :message, message)
+    end
+  end
+
+  defp authorize_message(conn, _) do
+    if Authorizer.can_access?(conn.assigns[:user], conn.assigns[:message]) do
+      conn
+    else
+      conn |> put_flash(:info, "You can't access that page") |> redirect(to: "/") |> halt()
+    end
+  end
+
+end
