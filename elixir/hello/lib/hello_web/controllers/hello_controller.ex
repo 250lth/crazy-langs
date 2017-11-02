cf3c9cfff34fd273195892f52308b0a14c58aa55
250lth <gang250lth@gmail.com>
2017-11-02 17:02:49 +0800

    add new controller and templates

---
+++ elixir/hello/lib/hello_web/controllers/hello_controller.ex
@@ -0,0 +1,14 @@
+defmodule HelloWeb.HelloController do
+  @moduledoc false
+  
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, "show.html", messenger: messenger
+  end
+
+end
---
+++ elixir/hello/lib/hello_web/views/hello_view.ex
@@ -0,0 +1,6 @@
+defmodule HelloWeb.HelloView do
+  @moduledoc false
+  
+  use HelloWeb, :view
+
+end
