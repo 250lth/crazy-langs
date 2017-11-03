9d2a8acaacfa79a5d048f5886194279cea307e1d
250lth <gang250lth@gmail.com>
2017-11-03 11:47:59 +0800

    Revert "add new controller and templates"
    This reverts commit cf3c9cf

--- elixir/hello/lib/hello_web/router.ex
+++ elixir/hello/lib/hello_web/router.ex
@@ -17,47 +17,7 @@ defmodule HelloWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/hello", HelloController, :index
-    get "/hello/:messenger", HelloController, :show
-
-    resources "/users", UserController
-    resources "/posts", PostController, only: [:index, :show]
-    resources "/comments", CommentController, except: [:delete]
-
-    resources "/users", UserController do
-      resources "/posts", PostController
-    end
-
-    resources "/reviews", ReviewController
-
   end
-
-  scope "/admin", HelloWeb.Admin, as: :admin do
-    pipe_through :browser
-
-    resources "/reviews", ReviewController
-    resources "/images", ImageController
-    resources "/users", UserController
-  end
-
-  scope "/api", HelloWeb do
-    pipe_through :api
-
-    resources "/reviews", ReviewController
-  end
-
-  pipeline :review_checks do
-    #plug :ensure_authenticated_user
-    #plug :ensure_user_owns_review
-  end
-
-  scope "/reviews", HelloWeb do
-    pipe_through :review_checks
-
-    resources "/", ReviewController
-  end
-
-  forward "/jobs", BackgroundJob.Plug
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloWeb do
