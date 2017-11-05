f63ad28db16cd3a83813bc8f55d3d9ef33bb3058
250lth <gang250lth@gmail.com>
2017-11-05 15:56:27 +0800

    channels in phoenix

---
+++ elixir/hello/lib/hello_web/channels/room_channel.ex
@@ -0,0 +1,28 @@
+defmodule HelloWeb.RoomChannel do
+  @moduledoc false
+  
+  use Phoenix.Channel
+
+  def join("room:lobby", _message, socket) do
+    { :ok, socket }
+  end
+  def join("room:" <> _private_room_id, _params, _socket) do
+    { :error, %{reason: "unauthorized"}}
+  end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{body: body}
+    {:noreply, socket}
+  end
+
+  intercept ["user_joined"]
+
+  def handle_out("user_joined", msg, socket) do
+    if User.ignoring?(socket.assigns[:user], msg.user_id) do
+      {:noreply, socket}
+    else
+      push socket, "user_joined", msg
+    end
+  end
+
+end
--- elixir/hello/lib/hello_web/channels/user_socket.ex
+++ elixir/hello/lib/hello_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule HelloWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", HelloWeb.RoomChannel
+  channel "room:*", HelloWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
