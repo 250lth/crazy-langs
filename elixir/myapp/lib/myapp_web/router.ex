694db111ecd4b8959715697719a76013667834e4
250lth <gang250lth@gmail.com>
2017-11-13 15:08:07 +0800

    sample of phoenix api

---
+++ elixir/myapp/lib/myapp_web/router.ex
@@ -0,0 +1,65 @@
+defmodule MyappWeb.Router do
+  use MyappWeb, :router
+  use Coherence.Router
+  use PhoenixOauth2Provider.Router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  pipeline :public do
+    plug Coherence.Authentication.Session
+  end
+
+  pipeline :protected do
+    plug Coherence.Authentication.Session, protected: true
+  end
+
+  pipeline :api_auth do
+    plug ExOauth2Provider.Plug.VerifyHeader, realm: "Bearer"
+    plug ExOauth2Provider.Plug.EnsureAuthenticated
+  end
+
+  pipeline :oauth_public do
+    plug :put_secure_browser_headers
+  end
+
+  scope "/" do
+    pipe_through :oauth_public
+    oauth_routes :public
+  end
+
+  scope "/" do
+    pipe_through [:browser, :public]
+    coherence_routes()
+  end
+
+  scope "/" do
+    pipe_through [:browser, :protected]
+    coherence_routes :protected
+  end
+
+  scope "/", MyappWeb do
+    pipe_through [:browser, :public] # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  scope "/api/v1", MyappWeb.API.V1 do
+    pipe_through :api_oauth
+    resources "/accounts", UserController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", MyappWeb do
+  #   pipe_through :api
+  # end
+end
