5eedaec987385e574500fa1df967fc87ea8c6a17
250lth <gang250lth@gmail.com>
2017-11-14 14:36:00 +0800

    use GenServer to implement the game

---
+++ elixir/islands_engine/lib/game.ex
@@ -0,0 +1,79 @@
+defmodule IslandsEngine.Game do
+  use GenServer
+  alias IslandsEngine.{Game, Player}
+  alias IslandsEngine.{Coordinate, IslandSet, Island}
+
+  defstruct player1: :none, player2: :none
+
+  def start_link(name) when not is_nil name do
+    GenServer.start_link(__MODULE__, name)
+  end
+
+  def init(name) do
+    {:ok, player1} = Player.start_link(name)
+    {:ok, player2} = Player.start_link()
+    {:ok, %Game{player1: player1, player2: player2}}
+  end
+
+  def call_demo(game) do
+    GenServer.call(game, :demo)
+  end
+
+  def cast_demo(game) do
+    GenServer.cast(game, :demo)
+  end
+
+  def add_player(pid, name) when name != nil do
+    GenServer.call(pid, {:add_player, name})
+  end
+
+  def set_island_coordinates(pid, player, island, coordinates) when is_atom player and is_atom island do
+    GenServer.call(pid, {:set_island_coordinates, player, island, coordinates})
+  end
+
+  def guess_coordinate(pid, player, coordinate) when is_atom player and is_atom coordinate do
+    GenServer.call(pid, {:guess, player, coordinate})
+  end
+
+  ### CALLBACKS
+  def handle_info(:first, state) do
+    IO.puts "This message has been handled by handle_info/2, matching on :first."
+    {:noreply, state}
+  end
+
+  def handle_call(:demo, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call({:add_player, name}, _from, state) do
+    Player.set_name(state.player2, name)
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:set_island_coordinates, player, island, coordinates}, _from, state) do
+    state
+    |> Map.get(player)
+    |> Player.set_island_coordinates(island, coordinates)
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:guess, player, coordinate}, _from, state) do
+    opponent = opponent(state, player)
+    opponent_board = Player.get_board(opponent)
+    response = Player.guess_coordinate(opponent_board, coordinate)
+    {:reply, response, state}
+  end
+
+  def handle_cast(:demo, state) do
+    {:noreply, %{state | test: "new value"}}
+  end
+
+  ### private funcs
+  defp opponent(state, :player1) do
+    state.player2
+  end
+
+  defp opponent(state, _player2) do
+    state.player1
+  end
+end
