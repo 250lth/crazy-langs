72243d079486d593b4c9ae79c03064818f687aff
250lth <gang250lth@gmail.com>
2017-11-17 14:50:43 +0800

    implementation of a supervisor using GenServer

---
+++ elixir/thy_supervisor/lib/thy_worker.ex
@@ -0,0 +1,17 @@
+defmodule ThyWorker do
+  @moduledoc false
+  def start_link do
+    spawn(fn -> loop end)
+  end
+
+  def loop do
+    receive do
+      :stop -> :ok
+
+      msg ->
+        IO.inspect msg
+        loop
+    end
+  end
+
+end
