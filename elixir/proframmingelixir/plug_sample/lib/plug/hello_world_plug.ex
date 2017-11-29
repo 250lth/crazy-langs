f9f9457a473fff9a74e79f9e6d33e305b92f95db
250lth <gang250lth@gmail.com>
2017-11-29 10:17:49 +0800

    sample of elixir plug -- the specification for composable modules between web applications

---
+++ elixir/proframmingelixir/plug_sample/lib/plug/hello_world_plug.ex
@@ -0,0 +1,14 @@
+defmodule HelloWorldPlug do
+  @moduledoc false
+  
+  import Plug.Conn
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    conn
+    |> put_resp_content_type("text/plain")
+    |> send_resp(200, "Hello World")
+  end
+
+end
---
+++ elixir/proframmingelixir/plug_sample/lib/plug_sample.ex
@@ -0,0 +1,18 @@
+defmodule PlugSample do
+  @moduledoc """
+  Documentation for PlugSample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugSample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
