546c548eb1184289e2a611fa0716c279cf2a00f6
trevor.liu <trevor.liu@edragonconnect.com>
2017-12-06 19:13:00 +0800

    add handlers for the channel

--- elixir/hello/lib/hello_web/channels/room_channel.ex
+++ elixir/hello/lib/hello_web/channels/room_channel.ex
@@ -11,14 +11,13 @@ defmodule HelloWeb.RoomChannel do
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast ! socket, "new_msg", %{body: body}
+    {:noreply, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  def handle_out("new_msg", payload, socket) do
+    push socket, "new_msg", payload
     {:noreply, socket}
   end
 
