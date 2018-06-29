0b7bb4741ea1807dfa413f6b666015a2c63c3257
250lth <gang250lth@gmail.com>
2018-06-29 15:27:37 +0800

    simple json lib

---
+++ haskell/real_world/json/Prettify.hs
@@ -0,0 +1,25 @@
+string :: String -> Doc
+string = enclose '"' '"' . hcat . map oneChar
+
+enclose :: Char -> Char -> Doc -> Doc
+enclose left right x = char left <> x <> char right
+
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                | otherwise -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\', b])
+
+hexEscape :: Char -> Doc
+hexEscape c = undefined
---
+++ haskell/real_world/json/PrettyJSON.hs
@@ -0,0 +1,8 @@
+import SimpleJSON
+
+renderJValue :: JValue -> Doc
+renderJValue (JBool True) = text "true"
+renderJValue (JBool False) = text "false"
+renderJValue JNull = text "null"
+renderJValue (JNumber num) = double num
+renderJValue (JString str) = string str
