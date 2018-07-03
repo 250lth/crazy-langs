27b29dfd5481f24e0489187f653cc8f15220ef2f
250lth <gang250lth@gmail.com>
2018-07-03 14:18:13 +0800

    sample of dynamic supervisor

---
+++ elixir/kv/lib/kv/supervisor.ex
@@ -0,0 +1,16 @@
+defmodule KV.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    children = [
+      {KV.Registry, name: KV.Registry},
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_all)
+  end
+end
